68ac7bbac97bbf3f3ae24e98ccac7235d7aef106
Reecon <reecon820@gmail.com>
2018-08-18 09:09:04 +0200

    add list of goals to overlay to keep track of what's in the list
    can be cut off by sizing the overlay

--- CombinedBar_StreamlabsSystem.py
+++ CombinedBar_StreamlabsSystem.py
@@ -16,7 +16,7 @@ ScriptName = "CombinedProgressBar"
 Website = "reecon820@gmail.com"
 Description = "Progress bar for goals that combines streamlabs donations and cheers."
 Creator = "Reecon820"
-Version = "0.4.0.0"
+Version = "0.4.1.0"
 
 #---------------------------
 #   Define Global Variables
